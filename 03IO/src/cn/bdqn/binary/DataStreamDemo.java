5e960c292984fa50e3be8ba6399a87d447fb02f1
xiaodoufu <501804292@qq.com>
2017-09-12 16:42:45 +0800

    2进制  输入输出流

---
+++ 03IO/src/cn/bdqn/binary/DataStreamDemo.java
@@ -0,0 +1,39 @@
+package cn.bdqn.binary;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 2进制 的输入 输出流 
+ */
+public class DataStreamDemo {
+
+	public static void main(String[] args) throws Exception {
+
+		InputStream in = new FileInputStream("e:/U2/music.mp3");
+		// 从电脑中读取某个2进制文件
+		DataInputStream dis = new DataInputStream(in);
+
+		OutputStream out = new FileOutputStream("e:/haha.mp3");
+		// 需要从内存中 放到电脑中的指定位置
+		DataOutputStream dos = new DataOutputStream(out);
+
+		int num = 0;
+		// 读取之后 直接 写入
+		while ((num = dis.read()) != -1) {
+			// 写入
+			dos.write(num);
+		}
+
+		dos.close();
+		dis.close();
+		out.close();
+		in.close();
+
+	}
+
+}
